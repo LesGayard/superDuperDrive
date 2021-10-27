ec53fbf57df1995d04d768b010926a8f2b5ab0fa
Leslie Gayard <leslie.123@hotmail.fr>
2021-10-27 08:39:41 +0200

    URL fixed

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/WelcomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/WelcomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/welcome")
+@RequestMapping("/")
 public class WelcomeController {
 
     private final UserService userService;
